8a0f563e8f4ca8f17e3b78f5cba293a70bb55c87
Tiuksha <dtyuksha@gmail.com>
2024-09-12 15:42:06 +0300

    Add files via upload

---
+++ rugsejo 12/main.cpp
@@ -0,0 +1,75 @@
+#include <inttypes.h>
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    //PIRMAS UZDUOTIS
+    cout << "------1 uzduotis-------" << endl;
+    string vardas = "Deividas";
+    string pavarde;
+    string pavadinimas = "Elektronikos ir Informatikos fakultetas";
+    int amzius, grupe, kursas;
+
+    pavarde = "Tiuksa";
+    amzius = 19;
+    grupe = 2;
+    kursas = 1;
+
+    cout << "Studento Vardas: " << vardas << endl;
+    cout << "Studento Pavarde: " << pavarde << endl;
+    cout << "Studento Amzius: " << amzius << endl;
+    cout << "Studento Grupe: " << grupe << endl;
+    cout << "Studento Kursas: " << kursas << endl;
+    cout << "Studiju Programu Pavadinimas: " << pavadinimas << endl;
+
+    //------------------------------------------
+
+    //ANTRAS UZDUOTIS
+    cout<<"------2 uzduotis-------"<<endl;
+    string klubas;
+    string savininkas;
+    string arena;
+    int metai, vietos;
+
+    klubas = "Madrido Real";
+    savininkas = "Florentino Perez";
+    arena = "Santiago Bernabeu Stadium";
+    metai = 1902;
+    vietos = 81044;
+
+    cout<<"Klubo Pavadinimas: "<<klubas<<endl;
+    cout<<"Ikurimo Metai: "<<metai<<endl;
+    cout<<"Savininkas: "<<savininkas<<endl;
+    cout<<"Stadionas: "<<arena<<endl;
+    cout<<"Vietu Skaicius: "<<vietos<<endl;
+    //-------------------
+    //TRECIAS UZDUOTIS
+    cout<<"------3 uzduotis-------"<<endl;
+    int marke, modelis, autometai, litrazas, spalva, Audi, A8, Juodas;
+    marke = Audi;
+    modelis = A8;
+    autometai = 2010;
+    litrazas = 4;
+    spalva = Juodas;
+
+    cout<<"Automobilis "<<marke<<endl<<"Yra pagamintas "<<autometai<<" metais"<<endl<<"Jo motoras "<<litrazas<<" litrazo"<<endl<<"Automobilis yra"<<spalva<<endl;
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
